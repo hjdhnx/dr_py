7869f8efd98aff7b5eab633f2c49d5235f44b6a3
hjdhnx <hjd124579>
2023-04-23 23:20:26 +0800

    合并一个更新

--- js/JRKAN直播.js
+++ js/JRKAN直播.js
@@ -12,8 +12,8 @@
 var rule = {
     title:'JRKAN直播',
     host:'http://www.jrkankan.com/?lan=1',
-	// JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com
-	// JRKAN网址发布:qiumi1314.com
+    // JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com
+    // JRKAN网址发布:qiumi1314.com
     url:'/fyclass',
     searchUrl:'',
     searchable:0,
@@ -31,14 +31,14 @@ var rule = {
     double:false,
     推荐:'*',
     // 一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',//play.sportsteam333.com
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'🏆'+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a.me&&href');items.push({desc:timer+'🏆'+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
-		"title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
-		"img":"img&&src",
-		"desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
-		"content":".sub_list ul&&Text",
-		"tabs":"js:TABS=['JRKAN直播']",
-		"lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
-	},
+        "title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
+        "img":"img&&src",
+        "desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
+        "content":".sub_list ul&&Text",
+        "tabs":"js:TABS=['JRKAN直播']",
+        "lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
+    },
     搜索:'',
 }
