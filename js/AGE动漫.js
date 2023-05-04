270e6c5e21b0be26f93fdd0b448c218b5406bf98
Wirili <Wirili@users.noreply.github.com>
2023-05-04 11:04:39 +0800

    Update AGE动漫.js

--- js/AGE动漫.js
+++ js/AGE动漫.js
@@ -1,7 +1,8 @@
 var rule = {
 	    title:'AGE动漫',
 	    // host:'https://www.agemys.cc',
-	    host:'https://www.agemys.net',
+	    //host:'https://www.agemys.net',
+	    host:'https://www.agemys.vip',
 	    url:'/catalog/all-all-all-fyclass-all-fyfilter',
 	    searchUrl:'/search?query=**&page=fypage',
 	    searchable:2,
