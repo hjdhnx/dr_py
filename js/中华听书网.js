82665aa48cefd7d2c59ba53343f963e27eb5edbd
tloook <tlooo.cute@gmail.com>
2023-05-22 19:27:23 +0800

    增加&更新源

--- js/中华听书网.js
+++ js/中华听书网.js
@@ -12,10 +12,10 @@ var rule = {
     timeout:5000,
     class_parse: '#news_nav&&li:has(a);a&&Text;a&&href;.*/(\\d+).html',
     play_parse:true,
-    lazy:'',
+    lazy:'js:input=input.replace("www","m")',
     limit:6,
     图片来源:'@Referer=https://www.tingshucn.com',
-    推荐:'.layout_right:eq(0)&&li;*;*;em&&Text;*',
+    推荐:'.layout_right:eq(1)&&li;*;*;em&&Text;*',
     一级:'.listBox&&li;img&&alt;img&&src;p:eq(0)&&span&&Text;a&&href',
     二级:{
         title:'h3&&Text;.webzi_top&&li:eq(4)&&Text',
@@ -23,7 +23,7 @@ var rule = {
         desc:'.webzi_top&&li:eq(1)&&span&&Text;;;.webzi_top&&li:eq(3)&&Text;.webzi_top&&li:eq(2)&&Text',
         content:'#idDIV--a&&Text',
         tabs:'.content_jrtop:eq(0)&&li',
-        lists:'.videourl:eq(#id)&&li',
+        lists:'.videourl:eq(#id)&&li'
     },
     搜索:'*',
 }
