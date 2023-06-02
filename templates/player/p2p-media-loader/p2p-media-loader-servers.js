474967fafce19df46005fd3bf4eefb5467b76bd0
晚风拂柳颜 <434857005@qq.com>
2023-06-02 15:28:58 +0800

    优化p2p播放器的wss配置

---
+++ templates/player/p2p-media-loader/p2p-media-loader-servers.js
@@ -0,0 +1,12 @@
+!function(e) {
+    this.trackerAnnounce = [
+        // "wss://signal.hitv.com",
+        // "wss://tracker.openwebtorrent.com",
+        // "wss://tracker.files.fm:7073",
+        // "ws://tracker.files.fm:7072",
+        // "wss://open.tube:443/tracker/socket",
+        // "ws://tracker.sloppyta.co",
+        "wss://tracker.fastcast.nz",
+        "wss://peertube.cpy.re",
+    ]
+}(this);
